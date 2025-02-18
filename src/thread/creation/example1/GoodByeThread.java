aeb91a1dc5f2cbcb35d66f47d1f522b10f5a4490
Carlo Contardi <c.contardi@spacecoding.it>
2025-02-18 16:50:13 +0100

    add: class that extends Thread

--- src/thread/creation/example1/GoodByeThread.java
+++ src/thread/creation/example1/GoodByeThread.java
@@ -5,6 +5,7 @@ public class GoodByeThread extends Thread{
     public GoodByeThread(String threadName){
         super(threadName);
     }
+    @Override
     public void run(){
         System.out.println("GoodBye!");
     }
